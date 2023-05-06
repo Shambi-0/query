ff07d08d23ea8c0a70ed7908577b73ddf6a8d2cb
Shambi <87273502+Shambi-0@users.noreply.github.com>
2023-05-05 19:54:28 -0500

    v9

---
+++ src/Instances/qHumanoid.ts
@@ -0,0 +1,51 @@
+import qProxy from "../Proxy";
+
+/**
+ * @description Flattens the provided function slightly.
+ */
+const F = (Callback: unknown) => (_: unknown, ... Arguments: unknown[]) => (Callback as any)(... Arguments);
+
+export class qHumanoid {
+    constructor(Basic: Humanoid) {
+        const Proxied = new qProxy(this, Basic, "Datatype")
+        
+        return Proxied.Logistic(Basic).Extend({
+
+            __metatable: Basic,
+            __tostring: () => Basic,
+
+            __eq: F((Value: unknown) => Basic === (Value as unknown))
+
+        }).Proxy as qHumanoid & Humanoid;
+    };
+
+    /**
+     * @description Heals the given humanoid.
+     * 
+     * @param {number} [Amount] Amount health you'd like to heal the humanoid by. (Undefined will heal the humanoid to it's max health.)
+     * @this {qHumanoid & Humanoid} The Humanoid that will be healed.
+     * @returns {qHumanoid & Humanoid} The Humanoid that has been healed.
+     * {@link https://www.npmjs.com/package/@rbxts/query}
+     */
+    public Heal(... Arguments: [ number? ]): qHumanoid & Humanoid {
+        const [, Basic, Amount] = [ ... Arguments, {} as Humanoid, 0 ];
+
+        Basic.Health += Amount ? Amount : (Basic.MaxHealth - Basic.Health);
+        return this as unknown as qHumanoid & Humanoid;
+    };
+
+    /**
+     * @description Kills the given humanoid. (Same as: "Humanoid.Health = 0")
+     * 
+     * @this {qHumanoid & Humanoid} The Humanoid that will be killed.
+     * @returns {qHumanoid & Humanoid} The Humanoid that has been killed.
+     * {@link https://www.npmjs.com/package/@rbxts/query}
+     */
+    public Kill(... Arguments: []): qHumanoid & Humanoid {
+        const [, Basic] = [ ... Arguments, undefined, {} as Humanoid ];
+
+        Basic.Health = 0;
+
+        return this as unknown as qHumanoid & Humanoid;
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
+// Extended Datatypes
 import { qNumber } from "./Datatypes/qNumber";
 import { qString } from "./Datatypes/qString";
 
+// Extended Instances
+import { qHumanoid } from "./Instances/qHumanoid";
+
 type Extended =
+    // Datatypes
     ((arg0: number) => (qNumber & number)) & // String[s]
-    ((arg0: string) => (qString & string)); // Number[s]
+    ((arg0: string) => (qString & string)) & // Number[s]
+    
+    // Instances
+    ((arg0: Humanoid) => (qHumanoid & Humanoid));
+
 
 /**
  * @description Provides an "extended" version of whatever instance or datatype you pass-through it.
  * 
  * @param {Type} Input The original datatype which you intend to query or extend.
- * @returns {Type} The extended
+ * @returns {Type} The extended version of the input.
  */
 const Default: Extended = ((Input: defined) => {
     let Output;
